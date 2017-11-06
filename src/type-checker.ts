4c585ed7eb9864f9069ac7cd6de41aba7ac67418
Gonza Gluzman <dggluz@gmail.com>
2017-11-06 12:21:57 -0300

    chore(prettier): update prettier config and ran over source code

--- src/type-checker.ts
+++ src/type-checker.ts
@@ -1,92 +1,92 @@
-type IType = 'string' | 'boolean' | 'object' | 'number' | 'undefined'
+type IType = 'string' | 'boolean' | 'object' | 'number' | 'undefined';
 export interface IMapOfContracts {
-  [propName: string]: IContract<any>
+    [propName: string]: IContract<any>;
 }
-export type IContract<T> = (x: T) => T
-type IThrowFn = (x: string) => never
+export type IContract<T> = (x: T) => T;
+type IThrowFn = (x: string) => never;
 
 const contract = <T>(type: IType): IContract<T> => x => {
-  if (typeof x !== type) {
-    throw new TypeError(
-      `${type} expected, but ${typeof x} was found (with value "${x}").`
-    )
-  }
-  return x
-}
+    if (typeof x !== type) {
+        throw new TypeError(
+            `${type} expected, but ${typeof x} was found (with value "${x}").`
+        );
+    }
+    return x;
+};
 
-const memberOf = <T>(element: T, arr: T[]) => arr.indexOf(element) !== -1
+const memberOf = <T>(element: T, arr: T[]) => arr.indexOf(element) !== -1;
 
 const checkAllIncluded = (arrA: string[], arrB: string[], throwFn: IThrowFn) =>
-  arrA.map(anElement => {
-    if (!memberOf(anElement, arrB)) {
-      throwFn(anElement)
-    }
-    return anElement
-  })
+    arrA.map(anElement => {
+        if (!memberOf(anElement, arrB)) {
+            throwFn(anElement);
+        }
+        return anElement;
+    });
 
 export const objOf = <T extends object = any>(
-  contractsMap: IMapOfContracts
+    contractsMap: IMapOfContracts
 ): IContract<T> => {
-  contractsMap = obj(contractsMap)
-  const contractKeys = Object.keys(contractsMap)
+    contractsMap = obj(contractsMap);
+    const contractKeys = Object.keys(contractsMap);
 
-  const throwExtraProp: IThrowFn = (propName: string) => {
-    throw new TypeError(`Extra property "${propName}"`)
-  }
-  const throwMissingProp: IThrowFn = (propName: string) => {
-    throw new TypeError(`Missing property "${propName}"`)
-  }
+    const throwExtraProp: IThrowFn = (propName: string) => {
+        throw new TypeError(`Extra property "${propName}"`);
+    };
+    const throwMissingProp: IThrowFn = (propName: string) => {
+        throw new TypeError(`Missing property "${propName}"`);
+    };
 
-  return (target: T) => {
-    const targetKeys = Object.keys(target)
-    checkAllIncluded(targetKeys, contractKeys, throwExtraProp)
-    checkAllIncluded(contractKeys, targetKeys, throwMissingProp)
+    return (target: T) => {
+        const targetKeys = Object.keys(target);
+        checkAllIncluded(targetKeys, contractKeys, throwExtraProp);
+        checkAllIncluded(contractKeys, targetKeys, throwMissingProp);
 
-    contractKeys.forEach(aContractKey => {
-      const aContract = contractsMap[aContractKey]
-      const prop = target[aContractKey as keyof T]
-      try {
-        aContract(prop)
-      } catch (e) {
-        if (!(e instanceof TypeError)) {
-          throw e
-        }
-        throw new TypeError(`[${aContractKey}]: ${e.message}`)
-      }
-    })
+        contractKeys.forEach(aContractKey => {
+            const aContract = contractsMap[aContractKey];
+            const prop = target[aContractKey as keyof T];
+            try {
+                aContract(prop);
+            } catch (e) {
+                if (!(e instanceof TypeError)) {
+                    throw e;
+                }
+                throw new TypeError(`[${aContractKey}]: ${e.message}`);
+            }
+        });
 
-    return target
-  }
-}
+        return target;
+    };
+};
 
 export const arr: IContract<any[]> = xs => {
-  if (!Array.isArray(xs)) {
-    throw new TypeError(
-      `Array expected, but ${typeof xs} was found (with value "${xs}").`
-    )
-  }
-  return xs
-}
+    if (!Array.isArray(xs)) {
+        throw new TypeError(
+            `Array expected, but ${typeof xs} was found (with value "${xs}").`
+        );
+    }
+    return xs;
+};
 
 export const arrOf = <T>(contract: IContract<T>): IContract<T[]> => xs => {
-  arr(xs)
-  return [].map.call(xs, contract)
-}
+    arr(xs);
+    return [].map.call(xs, contract);
+};
 
 // enum (we can't use 'enum' identifier since it's a reserved keyword)
 export const oneOf = <T>(...validValues: T[]): IContract<T> => x => {
-  if (!memberOf(x, validValues)) {
-    throw new TypeError(
-      `Expected one of: [${validValues
-        .map(x => `"${x.toString()}"`)
-        .join(', ')}], but the value "${x}" was found.`
-    )
-  }
-  return x
-}
+    if (!memberOf(x, validValues)) {
+        throw new TypeError(
+            `Expected one of: [${validValues
+                .map(x => `"${x.toString()}"`)
+                .join(', ')}], but the value "${x}" was found.`
+        );
+    }
+    return x;
+};
 
-export const bool = contract<boolean>('boolean')
-export const num = contract<number>('number')
-export const str = contract<string>('string')
-export const obj = contract<any>('object')
-export const undef = contract<undefined>('undefined')
+export const bool = contract<boolean>('boolean');
+export const num = contract<number>('number');
+export const str = contract<string>('string');
+export const obj = contract<any>('object');
+export const undef = contract<undefined>('undefined');
--- test/arr-of.test.ts
+++ test/arr-of.test.ts
@@ -1,28 +1,30 @@
-import { arrOf, str, num } from '../src/type-checker'
+import { arrOf, str, num } from '../src/type-checker';
 
 /**
  * arrOf typings
  */
 describe('`arrOf` contract', () => {
-  it('`arrOf(<string>)(x)` returns `x` when it is `x` an array of strings', () => {
-    const arrOfStringsContract = arrOf(str)
-    const strArr = ['a', 'b', 'c']
-    expect(arrOfStringsContract(strArr)).toEqual(strArr)
-  })
+    it('`arrOf(<string>)(x)` returns `x` when it is `x` an array of strings', () => {
+        const arrOfStringsContract = arrOf(str);
+        const strArr = ['a', 'b', 'c'];
+        expect(arrOfStringsContract(strArr)).toEqual(strArr);
+    });
 
-  it('`arrOf(<number>)(x)` returns `x` when it is `x` an array of numbers', () => {
-    const arrOfNumbersContract = arrOf(num)
-    const numArr = [1, 2, 3]
-    expect(arrOfNumbersContract(numArr)).toEqual(numArr)
-  })
+    it('`arrOf(<number>)(x)` returns `x` when it is `x` an array of numbers', () => {
+        const arrOfNumbersContract = arrOf(num);
+        const numArr = [1, 2, 3];
+        expect(arrOfNumbersContract(numArr)).toEqual(numArr);
+    });
 
-  it('`arrOf(<any>)(x)` throws TypeError if `x` is not an array', () => {
-    const arrOfStringsContract = arrOf(str)
-    expect(() => arrOfStringsContract('d' as any)).toThrowError(TypeError)
-  })
+    it('`arrOf(<any>)(x)` throws TypeError if `x` is not an array', () => {
+        const arrOfStringsContract = arrOf(str);
+        expect(() => arrOfStringsContract('d' as any)).toThrowError(TypeError);
+    });
 
-  it('`arrOf(<any>)(x)` throws TypeError if `x` is an array of different type', () => {
-    const arrOfStringsContract = arrOf(str)
-    expect(() => arrOfStringsContract([1, 2, 3] as any)).toThrowError(TypeError)
-  })
-})
+    it('`arrOf(<any>)(x)` throws TypeError if `x` is an array of different type', () => {
+        const arrOfStringsContract = arrOf(str);
+        expect(() => arrOfStringsContract([1, 2, 3] as any)).toThrowError(
+            TypeError
+        );
+    });
+});
